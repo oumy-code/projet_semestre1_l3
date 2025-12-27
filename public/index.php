f15230b1b6166ae6a797ac18883887a27ddd55cc
oumy-code <princesselebou12@gmail.com>
2025-12-27 15:28:09 +0000

    Fix: Configure trusted proxies for CSRF

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    // Gestion des Trusted Proxies pour Render
+    // Cela permet à Symfony de comprendre que le HTTPS est activé
+    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+    if ($trustedProxies) {
+        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+    }
+
+    return $kernel;
 };
